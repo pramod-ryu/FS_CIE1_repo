a63d2173468cd24f70764f3f7a2f44cdb1399650
Pramod <bhatpramod@gmail.com>
2023-08-28 14:27:50 +0530

    Initial setup

---
+++ calc.js
@@ -0,0 +1,7 @@
+function add()
+{
+n1=Number(document.getElementById("num1").value);
+n2=Number(document.getElementById("num2").value);
+result=n1+n2;
+document.getElementById("res").value=result;
+}
