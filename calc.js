bea87829e0b973929f5ee2623ad1cec033265724
Pramod <bhatpramod@gmail.com>
2023-08-28 14:46:49 +0530

    Included all calculations

--- calc.js
+++ calc.js
@@ -5,3 +5,42 @@ n2=Number(document.getElementById("num2").value);
 result=n1+n2;
 document.getElementById("res").value=result;
 }
+
+function sub()
+{
+n1=Number(document.getElementById("num1").value);
+n2=Number(document.getElementById("num2").value);
+result=n1-n2;
+document.getElementById("res").value=result;
+}
+
+function mul()
+{
+n1=Number(document.getElementById("num1").value);
+n2=Number(document.getElementById("num2").value);
+result=n1*n2;
+document.getElementById("res").value=result;
+}
+
+function div()
+{
+n1=Number(document.getElementById("num1").value);
+n2=Number(document.getElementById("num2").value);
+if (n2==0)
+{
+alert("Divide by zero!!");
+clr();
+}
+else
+{
+result=n1/n2;
+document.getElementById("res").value=result;
+}
+}
+
+function clr()
+{
+document.getElementById("num1").value="";
+document.getElementById("num2").value="";
+document.getElementById("res").value="";
+}
